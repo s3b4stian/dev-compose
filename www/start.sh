a71e492810e308a51c040d755111a6ce367b88b0
s3b4stian <sebastian.rapetti@tim.it>
2020-03-13 19:19:32 +0100

    dev updates

--- www/start.sh
+++ www/start.sh
@@ -6,10 +6,5 @@
 usermod -u $WWW_UID www-data
 groupmod -g $WWW_GID www-data
 
-# Very bad solution but now in the only available
-mysql -uroot -ppassword -hmysql -e "CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED WITH caching_sha2_password BY 'Admin1Admin1'"
-mysql -uroot -ppassword -hmysql -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%'"
-mysql -uroot -ppassword -hmysql -e "FLUSH PRIVILEGES"
-
 # Start supervisord and services
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
